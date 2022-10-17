de58ce21ee53f8247a2d821e9c2efc80a6ee57b3
KowsarRahman <s3825159@student.rmit.edu.au>
2022-10-18 05:29:41 +1100

    firebase-authentication done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { app , database} from './firebaseConfig';
+import { getAuth, createUserWithEmailAndPassword , signInWithEmailAndPassword} from "firebase/auth";
 
 function App() {
+
+  let auth = getAuth();
+
+  const [data, setData] = useState({});
+
+  const handleInput = (event) => {
+    let newInput = { [event.target.name]: event.target.value };
+
+    setData({ ...data, ...newInput });
+
+  };
+
+  const handleSubmit = () => {
+    signInWithEmailAndPassword(auth, data.email, data.password)
+    .then((response) => {
+      console.log(response.user);
+    })
+    .catch((error) => {
+      console.log(alert(error.message));
+    });
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <input 
+     name="email"
+     placeholder="Email"
+     onChange={(event) => handleInput(event)}
+     />
+     <input 
+     name="password"
+     placeholder="Password"
+     onChange={(event) => handleInput(event)}
+     />
+      <button onClick={handleSubmit}>Log In</button>
     </div>
   );
 }
---
+++ src/firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAD2nfz-QKG2K-Rf3ndzao_Qg_6O04Uamo",
+  authDomain: "react-firebase-12bc9.firebaseapp.com",
+  projectId: "react-firebase-12bc9",
+  storageBucket: "react-firebase-12bc9.appspot.com",
+  messagingSenderId: "727062088160",
+  appId: "1:727062088160:web:f7c268ac4d40835f128385",
+  measurementId: "G-KHY6TDN8D6"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const database = getFirestore(app);
